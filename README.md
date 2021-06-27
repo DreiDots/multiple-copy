6a2901b5a64f8d70873806bf9da439a448b48481
yashnavgire <yashnavgire51@gmail.com>
2021-06-27 22:02:17 +0530

    added support for multiple_copy_paste

--- README.md
+++ README.md
@@ -1,65 +1,22 @@
-# multiple-copy README
+Extension for Multiple Copy Paste.
 
-This is the README for your extension "multiple-copy". After writing up a brief description, we recommend including the following sections.
 
-## Features
+Description : 
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+This extension helps with problem of having a single copy instance, By using this a user can copy multiple things and paste them accordingly without worrying about overwriting of previous data.
 
-For example if there is an image subfolder under your extension project workspace:
 
-\!\[feature X\]\(images/feature-x.png\)
+Usage : 
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+To copy the selected text :
 
-## Requirements
+1.Enter ctrl+shift+p  (to open command palette).
+2.Enter copymultiple command.
+3.Extension will prompt for number to copy current selected text. 
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
 
-## Extension Settings
+To paste the particular copied instance : 
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+1.Enter ctrl+shift+p  (to open command palette).
+2.Enter pastemultiple command.
+3.Extension will prompt for number to paste previously copied data allocated to that number.   
--- extension.js
+++ extension.js
@@ -2,8 +2,11 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const fs = require('fs')
+const path = require('path');
+const { fileURLToPath } = require('url');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -14,65 +17,104 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "multiple-copy" is now active!');
 
+	var copy_dir = path.dirname(__dirname)
+	var copy_file = path.join(copy_dir,'.output.txt')
+	var resource_cnt = 0
 	//create hidden file for copy 
-	fs.writeFile('Output.txt',"", (err) => {
+	if(!fs.existsSync(copy_file)) {
+		fs.writeFile(copy_file,JSON.stringify({}), (err) => {
+			// In case of a error throw err.
+			if (err){
+				console.log("error occured"); 
+				throw err;
+			}
+		})
+	}
 	
-		// In case of a error throw err.
-		if (err){
-			console.log("error occured"); 
-			throw err;
-		}
-	})
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('multiple-copy.copymultiple', function () {
 			// The code you place here will be executed every time your command is executed
-			var editor = vscode.window.activeTextEditor;
-			if (!editor) {
-				return; // No open text editor
-			}
-			// Display a message box to the user
-			var selection = editor.selection;
-			var text = editor.document.getText(selection);
-			// console.log(text)
-			fs.appendFile('Output.txt',text,(err)=>{
+			vscode.window.showInputBox({
+				placeholder: 'Example: 1', // Placeholder
+				prompt: 'Type copy/paste number', // An indication of what to do
+				value: '0' // A value by default. In this case leave it empty
+			  }).then(value => {
+					// Use your parser here
+					resource_cnt = value
+
+					var editor = vscode.window.activeTextEditor;
+					if (!editor) {
+						return; // No open text editor
+					}
+					// Display a message box to the user
+					var selection = editor.selection;
+					var text = editor.document.getText(selection);
+					var data = null
+					// console.log(text)
+					fs.readFile(copy_file,function(err,content){
+						data = JSON.parse(content)
+						
+						var key = 'copy' + resource_cnt.toString()
+						data[key] = {
+							'value' : text
+						}
+						if(data!=null){
+							fs.writeFile(copy_file,JSON.stringify(data),(err)=>{
+								console.log(err)
+							})
+						}
+					})
+					vscode.window.showInformationMessage(text);
+			});
 				
-			})
-				
-			vscode.window.showInformationMessage(text);
+			
 	});
 
 	let pastable = vscode.commands.registerCommand('multiple-copy.pastemultiple', function () {
 		// The code you place here will be executed every time your com
 		var editor = vscode.window.activeTextEditor;
 		//output.txt read -> insert at current_cursor_position location
-		fs.readFile('Output.txt','utf8',(err,data)=>{
-			// console.log(data)
-			
-			// the Position object gives you the line and character where the cursor is
-				console.log("before")
-				const position = editor.selection.active;
-				console.log("after")
-				console.log(position._line)
-
-				editor.edit((editBuilder) =>
-				editBuilder.insert(
-						new vscode.Position(position._line + 1, 0),
-						data
-					)
-				);
-
-				vscode.window.showInformationMessage(data);
-				vscode.window.showInformationMessage(position);
-		})
-
-
-
 		
-		vscode.window.showInformationMessage("pastable");
-	});
+		vscode.window.showInputBox({
+			placeholder: 'Example: 1', // Placeholder
+			prompt: 'Type copy/paste number', // An indication of what to do
+			value: '0' // A value by default. In this case leave it empty
+		}).then(value => {
+				fs.readFile(copy_file,'utf8',(err,data)=>{
+				// console.log(data)
+				var readData = JSON.parse(data)
+
+				var key = 'copy' + value.toString()
+
+				if(readData[key]!==null || readData[key]!==undefined){
+						
+					// the Position object gives you the line and character where the cursor is
+					
+					var pasteData = readData[key]['value']
+					const position = editor.selection.active;
+					// console.log(position)
+
+					editor.edit((editBuilder) =>
+					editBuilder.insert(
+							new vscode.Position(position._line , position._character),
+							pasteData
+						)
+					);
+
+					// vscode.window.showInformationMessage(data);
+					// vscode.window.showInformationMessage(position);	
+				}
+				else{
+						vscode.window.showInformationMessage("paste for this number not available..please check if copied with this key");
+					}
+			})		
+			// vscode.window.showInformationMessage("pastable");
+		});
+	})
+		
 
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(pastable);
